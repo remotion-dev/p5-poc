aafe3307f299cb83cb6a67d412264b49da8de312
Jonny Burger <jonathanburger11@gmail.com>
2021-12-03 13:51:57 +0100

    add demo code

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,33 @@
-export const MyComposition = () => {
-	return null;
+import {useCallback} from 'react';
+import {useRef} from 'react';
+import React from 'react';
+import {ProcessingCanvas, P5Canvas} from '@remotion/p5';
+import {useCurrentFrame, useVideoConfig} from 'remotion';
+
+export const Demo: React.FC = () => {
+	const ref = useRef<P5Canvas>(null);
+	const frame = useCurrentFrame();
+	const {height, width} = useVideoConfig();
+
+	const draw = useCallback(() => {
+		const p5 = ref.current?.getSketch();
+		if (!p5) {
+			return;
+		}
+		p5.clear();
+
+		p5.rotateZ(frame * 0.1);
+		p5.rotateX(frame * 0.1);
+		p5.torus(100, 20);
+	}, [frame]);
+
+	return (
+		<ProcessingCanvas
+			ref={ref}
+			renderer="webgl"
+			width={width}
+			height={height}
+			draw={draw}
+		/>
+	);
 };
--- src/Video.tsx
+++ src/Video.tsx
@@ -1,12 +1,12 @@
 import {Composition} from 'remotion';
-import {MyComposition} from './Composition';
+import {Demo} from './Composition';
 
 export const RemotionVideo: React.FC = () => {
 	return (
 		<>
 			<Composition
 				id="Empty"
-				component={MyComposition}
+				component={Demo}
 				durationInFrames={60}
 				fps={30}
 				width={1280}
